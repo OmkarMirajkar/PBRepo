eca04bd4eeca68bf7c6bb85ed7f2642b95f1c4f8
Omkar Mirajkar <mirajkaromkarr@gmail.com>
2023-07-05 22:14:25 +0530

    <Omkar Mirajkar>commit1 by Omkar

---
+++ src/test/java/testpcg1/Test1.java
@@ -0,0 +1,13 @@
+package testpcg1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f()
+  {
+	  WebDriver driver=new ChromeDriver();
+  }
+}
